c8deded6425be8aabdc91cb4cd75831859e21f19
adossantos92 <adossantos92@gmail.com>
2019-04-04 14:05:38 -0400

    second commit

---
+++ Q3/cache/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='cache',
+    version='0.1',
+    author='Alexandre Dos Santos',
+    author_email='adossantos92@gmail.com',
+    description='cache class',
+    license='MIT',
+    packages=['cache']
+)
